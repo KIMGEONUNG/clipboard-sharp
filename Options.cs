dd470257a5625506df831219a99024dcf07ec6ab
comar <saywooong@gmail.com>
2020-04-20 14:58:56 +0900

    first commit

---
+++ Options.cs
@@ -0,0 +1,34 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToClipboard
+{
+    public class Options
+    {
+        [Option('e', "environment-variable", SetName = "env", Required = false, HelpText = "Copy target environment variable value to clipboard")]
+        public bool EnvVariable { get; set; }
+
+        [Option('c', "custom", SetName = "custom", Required = false, HelpText = "Copy target custom variable")]
+        public bool Custom { get; set; }
+
+        [Option("set", SetName = "set_custom", Required = false, HelpText = "Set custom macro clipboard")]
+        public bool SetCustom { get; set; }
+
+        [Option("delete", SetName = "delete_custom", Required = false, HelpText = "Delete custom")]
+        public bool DeleteCustom  { get; set; }
+
+        [Option('k', "key", Required = false, HelpText = "Set custom macro clipboard")]
+        public string CustomKey { get; set; }
+
+        [Option('v', "value", SetName = "set_custom", Required = false, HelpText = "Set custom macro clipboard")]
+        public string CustomValue { get; set; }
+
+        [Option('p', "print", SetName = "print", Required = false, HelpText = "Print current clipboard contents")]
+        public bool Print { get; set; }
+
+        [Option('i', "input", Required = false, HelpText = "input")]
+        public string Input { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using TextCopy;
+using CommandLine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace ToClipboard
+{
+    class Program
+    {
+        static string CONFIG_FILE_PATH = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "customs.json");
+        static string CONFIG_BASE_FORMAT = @"{}";
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+              .WithParsed(o =>
+              {
+                  if (o.Custom)
+                  {
+                      if (File.Exists(CONFIG_FILE_PATH))
+                      {
+                          JObject jo = JObject.Parse(File.ReadAllText(CONFIG_FILE_PATH));
+                          string val = jo[o.CustomKey].Value<string>();
+                          Clipboard.SetText(val);
+                          Console.WriteLine("Copy \"" + val + "\" To Clipboard");
+                      }
+                  }
+                  else if (o.DeleteCustom)
+                  {
+                      if (!File.Exists(CONFIG_FILE_PATH))
+                      {
+                          File.WriteAllText(CONFIG_FILE_PATH, CONFIG_BASE_FORMAT);
+                      }
+                      string key = o.CustomKey ?? throw new ArgumentNullException();
+                      JObject jo = JObject.Parse(File.ReadAllText(CONFIG_FILE_PATH));
+                      if(!jo.ContainsKey(key))
+                      {
+                          Console.WriteLine("No such a custom key exist");
+                          return;   
+                      }
+                      jo.Remove(key);
+                      File.WriteAllText(CONFIG_FILE_PATH, jo.ToString());
+                      Console.WriteLine("Successfully deleted");
+                  }
+                  else if (o.SetCustom)
+                  {
+                      if (!File.Exists(CONFIG_FILE_PATH))
+                      {
+                          File.WriteAllText(CONFIG_FILE_PATH, CONFIG_BASE_FORMAT);
+                      }
+                      string key = o.CustomKey ?? throw new ArgumentNullException();
+                      string value = o.CustomValue ?? throw new ArgumentNullException();
+                      JObject jo = JObject.Parse(File.ReadAllText(CONFIG_FILE_PATH));
+                      if(jo.ContainsKey(key))
+                      {
+                          Console.WriteLine("Same key already exits");
+                          return;   
+                      }
+                      jo[key] = value;
+
+                      File.WriteAllText(CONFIG_FILE_PATH, jo.ToString());
+                      Console.WriteLine("Successfully created");
+                  }
+                  else if (o.EnvVariable)
+                  {
+                      var dic = new Dictionary<string, string>();
+                      foreach (DictionaryEntry env in Environment.GetEnvironmentVariables())
+                      {
+                          dic.Add(env.Key.ToString(), env.Value.ToString());
+                      }
+                      try
+                      {
+                          var val = dic[o.Input];
+                          Clipboard.SetText(val);
+                          Console.WriteLine("Copy \"" + val + "\" To Clipboard");
+                      }
+                      catch (KeyNotFoundException)
+                      {
+                          Console.WriteLine("No Enviroment Variable");
+                      }
+                  }
+                  else if (o.Print)
+                  {
+                      string text = Clipboard.GetText();
+                      Console.WriteLine(text ?? "No clipboard contents"); 
+                  }
+              });
+        }
+    }
+}
